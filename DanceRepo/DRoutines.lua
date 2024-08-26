0cf9ca97cef9dc0e1e7e4736dcf83f8e26ebb269
Benjamin Paul <benjamin.ian.paul@gmail.com>
2024-08-25 22:02:47 -0400

    Initial commit

---
+++ DanceRepo/DRoutines.lua
@@ -0,0 +1,47 @@
+local t = Def.ActorFrame{};
+
+
+------- CHARACTER MOTION SELECTION -------	
+
+Motions = {
+	"AAAgain",
+	"Brain",
+	"Embarr",
+	"HareHare",
+	"Luka",
+	"River",
+	"Saturation",
+	"Specialist",
+	"World",
+	"_Dance0001",
+	"_Dance0002",
+	"_Dance0003",
+	"_Dance0004",
+	"_Dance0005",
+	"_Dance0006",
+	"_Dance0007",
+	"_Dance0008",
+	"_Dance0009",
+	"_Dance0010",
+	"_Dance0011",
+	"_Dance0012",
+	"_Dance0013",
+	"_Dance0014",
+	"_Dance0015",
+	"_Dance0016",
+}
+
+WarmUpMotions = {
+	"_Warmup0001",
+	"_Warmup0002",
+	"_Warmup0003",
+	"_Warmup0004",
+	"_Warmup0005",
+	"_Warmup0006",
+	"_Warmup0007",
+	"_Warmup0008",
+	"_Warmup0009",
+	"_Warmup0010",
+}
+
+return t;
